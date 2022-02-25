cde38b0d166f68291c6a2a309b845b95bf0813b4
om-guptaa <70843072+om-guptaa@users.noreply.github.com>
2022-02-25 11:56:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'topsisOm',         
   packages = ['topsisOm'],   
-  version = '0.3',      
+  version = '0.4',      
   license='MIT',       
   description = 'This package helps to get the topsis score from the given dataframe',   # Give a short description about your library
   author = 'Om Gupta',                   # Type in your name
   author_email = 'omstringtheory@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/om-guptaa',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/om-guptaa/topsisOm/archive/refs/tags/v_03.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/om-guptaa/topsisOm/archive/refs/tags/v_04.tar.gz',    # I explain this later on
   keywords = ['Topsis', 'Ranking'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
